c25cfe9b6003084ad883785e262c16c0d8be141e
Ingrid do Rio Schulze <ingriddorio@gmail.com>
2019-05-13 22:25:53 +0200

    start frontend for the search with the trefle api

--- src/search.js
+++ src/search.js
@@ -1,16 +1,41 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import axios from "./axios";
+
+function debouncer(fn, thisarg, time) {
+    let timeout;
+
+    return function() {
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+        timeout = setTimeout(fn.bind(thisarg, ...arguments), time);
+    };
+}
 
 export default class Search extends React.Component {
     constructor(props) {
         super(props);
         this.state = {};
+        this.search = debouncer(this.search, this, 350);
+    }
+
+    search(term) {
+        axios
+            .get("/api/search", {
+                params: { term: term }
+            })
+            .then(({ data }) =>
+                this.setState({
+                    results: data
+                })
+            );
     }
 
     render() {
         return (
             <div className="search">
                 <input
+                    onChange={e => this.search(e.target.value)}
                     type="text"
                     className="search-input"
                     placeholder="search"
